176d3575bf0e5364bd49cf7eef368bd2be020eab
Lethar1c <92082941+Lethar1c@users.noreply.github.com>
2024-11-03 09:15:41 +0300

    gitignore is working now

--- webapp/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import { searchSlice } from './slices/search-slice'
-
-export const store = configureStore({
-  reducer: {
-    search: searchSlice.reducer
-  }
-})
